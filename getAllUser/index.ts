c149350f922d7fe4cd3764d230b9eb4b44d018e7
Faysal Ahmad <faysalstat04@gmail.com>
2022-04-04 12:46:33 +0600

    checking new settings

--- getAllUser/index.ts
+++ getAllUser/index.ts
@@ -3,7 +3,6 @@ import * as db from "../lib/db-connector";
 import * as userDao from "../lib/dao/userDao";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     await db.init();
-    console.log(req);
     context.res = {
       body: {data : await userDao.getAllUser(req.query.id)},
     };
